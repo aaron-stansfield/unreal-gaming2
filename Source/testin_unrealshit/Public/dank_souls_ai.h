c23dc0bafcb3c20358dc0ceed5fec495ab3f778f
stansfield <aaronmstansfield@gmail.com>
2025-04-22 13:47:35 +0100

    a

---
+++ Source/testin_unrealshit/Public/dank_souls_ai.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "dank_souls_ai.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTIN_UNREALSHIT_API Adank_souls_ai : public AAIController
+{
+	GENERATED_BODY()
+	
+};
